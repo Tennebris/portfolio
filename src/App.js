7c368da853e23b31ae1f6ea17df1d17e16c9c890
Alisson santos <craftgames.alysson@gmail.com>
2020-12-05 16:42:48 -0300

    initial project portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
+
+import Logo from "./components/logo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="item" id="initial">
+				<nav class="navbar">
+					<a href="#initial"><Logo /></a>
+					<ul>
+						<li><a href="#home">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#skills">Skills</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</nav>
+				<main>
+					<Logo />
+				</main>
+	  		</div>
+			<div className="item" id="home">
+				{/* <Logo /> */}
+			</div>
+			<div className="item" id="about">
+				{/* <Logo /> */}
+			</div>
+			<div className="item" id="skills">
+				{/* <Logo /> */}
+			</div>
+			<div className="item" id="contact">
+				{/* <Logo /> */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
