12346a9788093134034b109f2d5a13c0c1f36047
Alisson santos <craftgames.alysson@gmail.com>
2020-12-12 08:10:30 -0300

    skills ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React, {useRef, useEffect, useState} from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from 'axios';
+
 import "./app.css";
-
 import img from "./assets/model.png"
-
-// https://api.github.com/users/tennebris/repos
-
 import Logo from "./components/logo";
 
+
 function App() {
+	const [repos, setRepos] = useState([]);
+	const skills = useRef();
+	const [isVisable, setIsVisable] = useState();
+	
+	function isInViewport(el) {
+		const rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	
+		);
+	}
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get('https://api.github.com/users/tennebris/repos');
+	
+			setRepos(response.data)
+		})();
+	},[]);
+
+	const variavel = {
+		'php':{
+			'--teste': '28%'
+		},
+		'python':{
+			'--teste': '87%'
+		},
+		'js':{
+			'--teste': '65%'
+		},
+		'reactjs':{
+			'--teste': '80%'
+		},
+		'rn':{
+			'--teste': '90%'
+		},
+		'nodejs':{
+			'--teste': '85%'
+		},
+	}
+
 	return (
-		<div className="container">
+		<div className="container" onScroll={() => {
+			if(isInViewport(skills.current)){
+				setIsVisable(true)
+			}else{
+				setIsVisable(false)
+			}
+		}} >
 			<div className="item" id="home">
 				<nav className="navbar">
 					<a href="#initial"><Logo /></a>
@@ -29,7 +77,7 @@ function App() {
 
 			<div className="item" id="about">
 				<div className="titlePage">
-					<h1>About</h1>	
+					<h1>About</h1>
 				</div>
 				<div className="texts">
 					<h1 className="title">Alisson Santos</h1>
@@ -46,17 +94,81 @@ function App() {
 
 
 			<div className="item" id="projects">
-				
+				<div className="titlePage">
+					<h1>Projects</h1>	
+				</div>
+				<table>
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nome</th>
+							<th>Linguagem</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+						repos.map((item) => (
+							<tr>
+								<td>{item.id}</td>
+								<td>{item.name}</td>
+								<td>{item.language}</td>
+								<td><a href={item.html_url}>Open</a></td>
+							</tr>
+						))
+						}
+					</tbody>
+				</table>
 			</div>
 
 
-			<div className="item" id="skills">
-				{/* <Logo /> */}
+			<div className="item" id="skills" ref={skills} >
+
+				<div className="titlePage">
+					<h1>Skills</h1>	
+				</div>
+
+				<div className="bars">
+					<label>PHP</label>
+					<div className="bar" >
+						<div className={isVisable ? "progress anime" : "progress animeReverse"} style={variavel.php} data={variavel.php["--teste"]}></div>
+					</div>
+					<label>python</label>
+					<div className="bar" >
+						<div className={isVisable ? "progress anime" : "progress animeReverse"} style={variavel.python} data={variavel.python["--teste"]}></div>
+					</div>
+					<label>javascript</label>
+					<div className="bar" >
+						<div className={isVisable ? "progress anime" : "progress animeReverse"} style={variavel.js} data={variavel.js["--teste"]}></div>
+					</div>
+					<label>reactjs</label>
+					<div className="bar" >
+						<div className={isVisable ? "progress anime" : "progress animeReverse"} style={variavel.reactjs} data={variavel.reactjs["--teste"]}></div>
+					</div>
+					<label>react native</label>
+					<div className="bar" >
+						<div className={isVisable ? "progress anime" : "progress animeReverse"} style={variavel.rn} data={variavel.rn["--teste"]}></div>
+					</div>
+					<label>nodejs</label>
+					<div className="bar" >
+						<div className={isVisable ? "progress anime" : "progress animeReverse"} style={variavel.nodejs} data={variavel.nodejs["--teste"]}></div>
+					</div>
+				</div>
+
 			</div>
 
 
 			<div className="item" id="contact">
-				{/* <Logo /> */}
+				<div className="titlePage">
+					<h1>Contact</h1>	
+				</div>
+
+				<div>
+					<input type="text"></input>
+					<input type="text"></input>
+					<input type="text"></input>
+					<input></input>
+				</div>
 			</div>
 		</div>
 	);
